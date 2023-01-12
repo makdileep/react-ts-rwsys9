55182fea7a1ffba23b8b115aae1bf5e57d0ce998
makdileep <47356928+makdileep@users.noreply.github.com>
2023-01-12 18:59:47 +0530

    1 file modified

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,30 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(({ handleSearchItem }) => {
   const [searchText, setSearchText] = useState('');
-
+  const inputRef = useRef();
   useEffect(() => {
-    const query =
-      searchText.length === 0 ? '' : `?orderBy="title"&equalTo="${searchText}"`;
-    fetch(
-      'https://react-hooks-6e5a0-default-rtdb.firebaseio.com/ingrediants.json' +
-        query
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngrediants = [];
-        for (let key in responseData) {
-          loadedIngrediants.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    const timer = setTimeout(() => {
+      if (searchText === inputRef.current.value) {
+        //Checking with current value after 500 milliseconds
+        const query =
+          searchText.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${searchText}"`;
+        fetch(
+          'https://react-hooks-6e5a0-default-rtdb.firebaseio.com/ingrediants.json' +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngrediants = [];
+            for (let key in responseData) {
+              loadedIngrediants.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            handleSearchItem(loadedIngrediants);
           });
-        }
-        handleSearchItem(loadedIngrediants);
-      });
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [searchText, handleSearchItem]);
 
   return (
@@ -34,6 +45,7 @@ const Search = React.memo(({ handleSearchItem }) => {
           <label>Filter by Title</label>
           <input
             type="text"
+            ref={inputRef}
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
